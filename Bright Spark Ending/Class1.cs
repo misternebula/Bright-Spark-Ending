529c4da2db9facd986d73f4bf86d2b6c7f2867ac
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-08-03 09:50:05 +0100

    Add project files.

---
+++ Bright Spark Ending/Class1.cs
@@ -0,0 +1,16 @@
+﻿using OWML.ModHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bright_Spark_Ending
+{
+    public class Class1 : ModBehaviour
+    {
+        void Start()
+        {
+
+        }
+    }
+}
